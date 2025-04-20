a742daa85c96bba62b2e9ee3e80820ee0fcde0b6
Tima4708 <gtgame81470@gmail.com>
2025-04-20 14:33:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,15 +12,20 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-    def update(self):
+    def update_r(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
-    def fire(self):
-        bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15)
-        bullets.add(bullet)
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 100:
+            self.rect.y += self.speed
+
+
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 100:
+            self.rect.y += self.speed
 
 
 back = (200, 255, 255)
@@ -33,9 +38,43 @@ game = True
 finish = False
 clock = time.Clock()
 FPS = 60
+
+racket1 = Player('racket.png', 30, 200, 40, 100, 5)
+racket2 = Player('racket.png', 520, 200, 40, 100, 5)
+ball = GameSprite('tenis_ball.png', 200, 200, 50, 50, 50)
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('Player 1 LOSE!!', True, (180, 0 , 0))
+lose2 = font.render('Player 2 LOSE!!', True, (180, 0 , 0))
+
+
+speed_x = 3
+speed_y = 3
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+    if finish != True:
+        window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+        if ball.rect.y > win_height -50 or ball.rect.y < 0:
+            speed_y *= -1
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+            game_over = True
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game_over = True
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
     display.update()
     clock.tick(FPS)
